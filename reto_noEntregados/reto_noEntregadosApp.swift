c812d2448f9abbd89e46766510159e8ed0580929
jimenagr <a01252605@itesm.mx>
2023-10-11 23:42:18 -0600

    Initial Commit

---
+++ reto_noEntregados/reto_noEntregadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  reto_noEntregadosApp.swift
+//  reto_noEntregados
+//
+//  Created by Jimena Gallegos on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct reto_noEntregadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
